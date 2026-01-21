677c70fee5dc8ec85b1940ed22ce3ace2db462fc
Aditya Kumar <adityakumar.455223@gmail.com>
2026-01-21 23:38:37 +0530

    Added new pattern files and reorganized existing files into folders

---
+++ Advance Pattern Qus/HollowRectang.java
@@ -0,0 +1,22 @@
+public class HollowRectang {
+
+    public static void pattern(int row ,int col){
+        for(int i = 1;i<=row;i++){
+            for(int j=1;j<=col;j++){
+
+                if (i==1 || i==row ||j==1 || j==col) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern(4, 5);
+    }
+    
+}
